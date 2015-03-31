a926124d3127f8bf964e4e633f2242fd3ce3e652
Kurt McAlpine <kurt@linux.com>
2015-03-31 20:51:16 +1300

    Opens property links in webbrowser

--- properties.py
+++ properties.py
@@ -3,6 +3,7 @@
 import urllib.request
 import re
 from bs4 import BeautifulSoup
+import webbrowser
 
 def get_urls(url):
     urls = list()
@@ -13,7 +14,8 @@ def get_urls(url):
     return urls
 
 def get_properties_trademe(price_start, price_end, keywords):
-    url = 'http://www.trademe.co.nz/Browse/CategoryAttributeSearchResults.aspx?search=1&cid=5748&sidebar=1&rptpath=350-5748-4233-&132=FLAT&selected135=7&selected136=89&134=1&135=7&136=89&216=0&216=0&217=0&217=0&153=' + keywords + '&122=0&122=1&59=' + price_start + '00' + '&59=' + price_end + '00' + '&178=0&178=0&sidebarSearch_keypresses=0&sidebarSearch_suggested=0'
+    keywords_str = '+'.join(map(str, keywords))
+    url = 'http://www.trademe.co.nz/Browse/CategoryAttributeSearchResults.aspx?search=1&cid=5748&sidebar=1&rptpath=350-5748-4233-&132=FLAT&selected135=7&selected136=89&134=1&135=7&136=89&216=0&216=0&217=0&217=0&153=' + keywords_str + '&122=0&122=1&59=' + price_start + '00' + '&59=' + price_end + '00' + '&178=0&178=0&sidebarSearch_keypresses=0&sidebarSearch_suggested=0'
 
     urls = get_urls(url)
     properties = list()
@@ -24,8 +26,12 @@ def get_properties_trademe(price_start, price_end, keywords):
     properties.sort()
     properties = list(set(properties))
 
+    for i in range(0, len(properties)):
+        properties[i] = 'http://www.trademe.co.nz' + properties[i]
+
     return properties
 
 if __name__ == '__main__':
-    urls = get_properties_trademe('250', '400', 'unfurnished')
-    print(urls)
+    urls = get_properties_trademe('250', '400', ['unfurnished'])
+    for url in urls:
+        webbrowser.open(url);
