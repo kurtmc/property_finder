b78894724792cd13a9d9cbe891633fa9366191bb
Kurt McAlpine <kurt@linux.com>
2015-12-29 23:02:16 +1300

    Tabs to spaces

--- properties.py
+++ properties.py
@@ -9,106 +9,106 @@ import blacklist
 import sys, getopt
 
 def get_urls(url):
-	urls = list()
-	try:
-		html_page = urllib.request.urlopen(url)
-	except urllib.error.URLError:
-		print("Timed out trying to get page")
-		exit(1)
-	soup = BeautifulSoup(html_page, "html.parser")
-	for link in soup.findAll('a'):
-		urls.append(link.get('href'))
-	return urls
+    urls = list()
+    try:
+        html_page = urllib.request.urlopen(url)
+    except urllib.error.URLError:
+        print("Timed out trying to get page")
+        exit(1)
+    soup = BeautifulSoup(html_page, "html.parser")
+    for link in soup.findAll('a'):
+        urls.append(link.get('href'))
+    return urls
 
 def get_properties_trademe(price_start, price_end, keywords=[]):
-	keywords_str = '+'.join(map(str, keywords))
-	url = 'http://www.trademe.co.nz/Browse/CategoryAttributeSearchResults.aspx?search=1&cid=5748&sidebar=1&rptpath=350-5748-4233-&132=FLAT&selected135=7&selected136=89&134=1&135=7&136=89&216=0&216=0&217=0&217=0&153=' + keywords_str + '&122=0&122=1&59=' + price_start + '00' + '&59=' + price_end + '00' + '&178=0&178=0&sidebarSearch_keypresses=0&sidebarSearch_suggested=0'
+    keywords_str = '+'.join(map(str, keywords))
+    url = 'http://www.trademe.co.nz/Browse/CategoryAttributeSearchResults.aspx?search=1&cid=5748&sidebar=1&rptpath=350-5748-4233-&132=FLAT&selected135=7&selected136=89&134=1&135=7&136=89&216=0&216=0&217=0&217=0&153=' + keywords_str + '&122=0&122=1&59=' + price_start + '00' + '&59=' + price_end + '00' + '&178=0&178=0&sidebarSearch_keypresses=0&sidebarSearch_suggested=0'
 
-	urls = get_urls(url)
-	properties = list()
-	for u in urls:
-		if u is not None and 'auction' in u:
-			properties.append(u)
+    urls = get_urls(url)
+    properties = list()
+    for u in urls:
+        if u is not None and 'auction' in u:
+            properties.append(u)
 
-	properties.sort()
-	properties = list(set(properties))
+    properties.sort()
+    properties = list(set(properties))
 
-	for i in range(0, len(properties)):
-		properties[i] = 'http://www.trademe.co.nz' + properties[i]
+    for i in range(0, len(properties)):
+        properties[i] = 'http://www.trademe.co.nz' + properties[i]
 
-	return properties
+    return properties
 
 def words_in_string(words, string):
-	for word in words:
-		if word_in_string(word, string):
-			return True
-	return False
+    for word in words:
+        if word_in_string(word, string):
+            return True
+    return False
 
 def word_in_string(word, string):
-	return re.compile(r'\b({0})\b'.format(word), flags=re.IGNORECASE).search(string)
+    return re.compile(r'\b({0})\b'.format(word), flags=re.IGNORECASE).search(string)
 
 # Stolen from stackoverflow (http://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text)
 def visible(element):
-	if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
-		return False
-	elif re.match('<!--.*-->', str(element)):
-		return False
-	return True
+    if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+        return False
+    elif re.match('<!--.*-->', str(element)):
+        return False
+    return True
 
 def print_help():
-	print("Search for properties in price range")
-	print("./properties -s <start price> -e <end price>")
-	print("Blacklist a property")
-	print("./properties -b <url>")
-	print("Print this message")
-	print("./properties -h")
+    print("Search for properties in price range")
+    print("./properties -s <start price> -e <end price>")
+    print("Blacklist a property")
+    print("./properties -b <url>")
+    print("Print this message")
+    print("./properties -h")
 
 if __name__ == '__main__':
-	# Command line options
-	start_price = None
-	end_price = None
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hs:e:b:", ["help", "start_price", "end_price", "blacklist"])
-	except getopt.GetoptError:
-		print_help()
-		sys.exit(2)
-	for opt, arg in opts:
-		if opt in ("-h", "--help"):
-			print_help()
-			sys.exit(2)
-		elif opt in ("-b", "--blacklist"):
-			blacklist.blacklist(arg)
-			print("Blacklisted " + arg);
-			sys.exit(0)
-		elif opt in ("-s", "--start_price"):
-			start_price = arg
-		elif opt in ("-e", "--end_price"):
-			end_price = arg
+    # Command line options
+    start_price = None
+    end_price = None
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hs:e:b:", ["help", "start_price", "end_price", "blacklist"])
+    except getopt.GetoptError:
+        print_help()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print_help()
+            sys.exit(2)
+        elif opt in ("-b", "--blacklist"):
+            blacklist.blacklist(arg)
+            print("Blacklisted " + arg);
+            sys.exit(0)
+        elif opt in ("-s", "--start_price"):
+            start_price = arg
+        elif opt in ("-e", "--end_price"):
+            end_price = arg
 
-	if start_price is None or end_price is None:
-		print_help()
-		sys.exit(1)
+    if start_price is None or end_price is None:
+        print_help()
+        sys.exit(1)
 
-	urls = get_properties_trademe(start_price, end_price)
-	properties = list()
-	for url in urls:
-		if not blacklist.is_blacklisted(url):
-			properties.append(Trademe_Property(url))
+    urls = get_properties_trademe(start_price, end_price)
+    properties = list()
+    for url in urls:
+        if not blacklist.is_blacklisted(url):
+            properties.append(Trademe_Property(url))
 
-	unfurnished = list()
-	for p in properties:
-		p_text = filter(visible, p.get_soup().findAll(text=True))
-		p_text = " ".join(p_text)
-		if not words_in_string(["furnished", "rented", "bed"], p_text):
-			unfurnished.append(p)
+    unfurnished = list()
+    for p in properties:
+        p_text = filter(visible, p.get_soup().findAll(text=True))
+        p_text = " ".join(p_text)
+        if not words_in_string(["furnished", "rented", "bed"], p_text):
+            unfurnished.append(p)
 
-	unfurnished = list(set(unfurnished))
+    unfurnished = list(set(unfurnished))
 
-	for p in unfurnished:
-		print("Title: " + p.get_title())
-		#print("Description:")
-		#print(p.get_description()[:200])
-		print("url: " + p.url)
-		print("Price: " + p.get_price())
-		print("Address: " + ", ".join(p.get_address().splitlines()))
-		print()
+    for p in unfurnished:
+        print("Title: " + p.get_title())
+        #print("Description:")
+        #print(p.get_description()[:200])
+        print("url: " + p.url)
+        print("Price: " + p.get_price())
+        print("Address: " + ", ".join(p.get_address().splitlines()))
+        print()
--- trademe_property.py
+++ trademe_property.py
@@ -2,51 +2,51 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 class Trademe_Property:
-	def __init__(self, url):
-		self.url = url
-		self.html_page = None
-		self.soup = None
+    def __init__(self, url):
+        self.url = url
+        self.html_page = None
+        self.soup = None
 
-	def build_string(self, soup):
-		result = ''
-		for s in soup.contents:
-			result = result + str(s)
-		return self.html_to_text(result.strip())
+    def build_string(self, soup):
+        result = ''
+        for s in soup.contents:
+            result = result + str(s)
+        return self.html_to_text(result.strip())
 
-	def get_soup(self):
-		if self.soup is None:
-			self.soup = BeautifulSoup(self.get_html(), "html.parser")
-		return self.soup
-		
+    def get_soup(self):
+        if self.soup is None:
+            self.soup = BeautifulSoup(self.get_html(), "html.parser")
+        return self.soup
+        
 
-	def html_to_text(self, html_string):
-		return html_string.replace("<br/>", "\n")
+    def html_to_text(self, html_string):
+        return html_string.replace("<br/>", "\n")
 
-	def get_attribute(self, tag, attrs={}):
-		soup = self.get_soup()
-		attribute = soup.find(tag, attrs)
-		return self.build_string(attribute)
+    def get_attribute(self, tag, attrs={}):
+        soup = self.get_soup()
+        attribute = soup.find(tag, attrs)
+        return self.build_string(attribute)
 
-	def get_description(self):
-		return self.get_attribute('div', {'id': 'ListingDescription_ListingDescription'})
-	
-	def get_price(self):
-		return self.get_attribute('li', {'id': 'ListingTitle_classifiedTitlePrice'})
+    def get_description(self):
+        return self.get_attribute('div', {'id': 'ListingDescription_ListingDescription'})
+    
+    def get_price(self):
+        return self.get_attribute('li', {'id': 'ListingTitle_classifiedTitlePrice'})
 
-	def get_address(self):
-		soup = self.get_soup()
-		address = soup.find('th', {'id': 'ListingAttributes_AttributesRepeater_ctl01_ltHeaderRow'}).parent.findNext('td')
-		return self.build_string(address)
+    def get_address(self):
+        soup = self.get_soup()
+        address = soup.find('th', {'id': 'ListingAttributes_AttributesRepeater_ctl01_ltHeaderRow'}).parent.findNext('td')
+        return self.build_string(address)
 
-	def get_title(self):
-		return self.get_attribute('h1', {'id': 'ListingTitle_title'})
+    def get_title(self):
+        return self.get_attribute('h1', {'id': 'ListingTitle_title'})
 
-	def get_html(self):
-		if self.html_page is None:
-			try:
-				self.html_page = urllib.request.urlopen(self.url)
-			except urllib.error.URLError:
-				print("An error occured trying to get page for url: " + self.url)
-		return self.html_page
-			
+    def get_html(self):
+        if self.html_page is None:
+            try:
+                self.html_page = urllib.request.urlopen(self.url)
+            except urllib.error.URLError:
+                print("An error occured trying to get page for url: " + self.url)
+        return self.html_page
+            
 
